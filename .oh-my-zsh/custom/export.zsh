14345af0fad01393a50f039c5380c67b60efacc1
ewwwdp <ewwdp@proton.me>
2024-10-21 20:26:24 +0300

    replaced nvm with fnm due to long startup time

--- .oh-my-zsh/custom/export.zsh
+++ .oh-my-zsh/custom/export.zsh
@@ -3,10 +3,12 @@ export DOTNET_ROOT=$HOME/.dotnet
 export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
 
-# nvm
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
 # rust
 . "$HOME/.cargo/env"
+
+# fnm
+FNM_PATH="/home/dpper/.local/share/fnm"
+if [ -d "$FNM_PATH" ]; then
+  export PATH="/home/dpper/.local/share/fnm:$PATH"
+  eval "`fnm env`"
+fi
