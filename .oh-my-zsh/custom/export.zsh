63ea37ffae9d212d4db1212ec9c6d010f355be28
ewwwdp <ewwdp@proton.me>
2024-12-20 14:14:55 +0200

    fix path

--- .oh-my-zsh/custom/export.zsh
+++ .oh-my-zsh/custom/export.zsh
@@ -7,8 +7,8 @@ export DOTNET_CLI_TELEMETRY_OPTOUT=1
 . "$HOME/.cargo/env"
 
 # fnm
-FNM_PATH="/home/dpper/.local/share/fnm"
+FNM_PATH="$HOME/.local/share/fnm"
 if [ -d "$FNM_PATH" ]; then
-  export PATH="/home/dpper/.local/share/fnm:$PATH"
+  export PATH="$HOME/.local/share/fnm:$PATH"
   eval "`fnm env`"
 fi
